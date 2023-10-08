9b2c09c24419dbed063259619cd61f1ff3ec6254
pink <66056077@kmitl.ac.th>
2023-10-08 12:27:32 +0700

    test_ex06

--- 25_test_cases/src/ex06.py
+++ 25_test_cases/src/ex06.py
@@ -3,7 +3,7 @@ Execise 6
 """
 
 
-def ordinal_suffix(param):
+def ordinal_suffix(number):
     """
     Return the ordinal suffix for a given number.
 
@@ -14,4 +14,16 @@ def ordinal_suffix(param):
         str: The ordinal suffix corresponding to the given number.
     """
     # TODO : complete this
-    pass
+    last_num = int(str(number)[-1:])
+    last_2num = int(str(number)[-2:])
+    len_num = (len(str(number)))
+    if last_num == 1 and last_2num != 11:
+        return f'{number}st'
+    elif last_num == 2 and last_2num != 12 :
+        return f'{number}nd'
+    elif last_num == 3 and last_2num != 13 :
+        return f'{number}rd'
+    else :
+        return f'{number}th'
+
+#%%
