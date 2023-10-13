322084986bee7b3bd3e1849ae5fa2c7ca237fd22
pink <66056077@kmitl.ac.th>
2023-10-13 21:42:48 +0700

    test ex20

--- 25_test_cases/src/ex20.py
+++ 25_test_cases/src/ex20.py
@@ -13,5 +13,12 @@ def is_leap_year(year):
     Returns:
         bool: True if the year is a leap year, False otherwise.
     """
-    # TODO : complete this
-    pass
+    # FIX : complete this
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
