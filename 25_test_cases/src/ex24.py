a1de4372eafb68350734d9e5ca687262e89c6963
pink <66056077@kmitl.ac.th>
2023-10-13 22:16:14 +0700

    test ex24

--- 25_test_cases/src/ex24.py
+++ 25_test_cases/src/ex24.py
@@ -16,5 +16,8 @@ def get_time_every_15_min():
     Returns:
         str: The generated time string.
     """
-    # TODO : complete this
-    pass
+    # FIX : complete this
+    for midday in ['am', 'pm']:
+        for hour in ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']:
+            for minutes in ['00', '15', '30', '45']:
+                print(hour + ':' + minutes + ' ' + midday)
