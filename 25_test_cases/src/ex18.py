2479f7f4b7afb65c1187da6ad2eb261133445f8c
pink <66056077@kmitl.ac.th>
2023-10-13 21:03:44 +0700

    test ex18

--- 25_test_cases/src/ex18.py
+++ 25_test_cases/src/ex18.py
@@ -4,9 +4,9 @@ Exercise 18 : Buy 8 get 1 free
 
 
 def get_cost_of_coffee(amountcoff, price):
-        freecoff = amountcoff // 9
-        diffprice = amountcoff - freecoff
-        return diffprice * price
+    freecoff = amountcoff // 9
+    diffprice = amountcoff - freecoff
+    return diffprice * price
 
 def get_cost_of_coffee_2(numcoff, price):
     freecoff = numcoff // 9
