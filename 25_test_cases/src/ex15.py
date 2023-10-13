55a74febfc5cad6cb2b09c0567dd8747731fb1e3
pink <66056077@kmitl.ac.th>
2023-10-13 19:50:04 +0700

    test ex15

--- 25_test_cases/src/ex15.py
+++ 25_test_cases/src/ex15.py
@@ -3,7 +3,7 @@ Exercise 15
 """
 
 
-def median(params):
+def median(num_list):
     """
     Calculate the median of a list of numbers.
 
@@ -13,4 +13,15 @@ def median(params):
     Returns:
         [int, None]: The median value of the list, or None if the list is empty.
     """
-    pass
+    num_list.sort()
+    cal_len = num_list.__len__()
+    index_median = cal_len//2
+
+    if cal_len == 0:
+        return None
+    elif cal_len % 2 == 0 :
+        cal_median = (num_list[index_median] + num_list[index_median-1])/2
+        return cal_median
+    else :
+        cal_median = num_list[index_median]
+        return cal_median
