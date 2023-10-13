d407214f3460dddd19ebb63ddd911fb96d51cf97
pink <66056077@kmitl.ac.th>
2023-10-13 21:25:02 +0700

    test ex19

--- 25_test_cases/src/ex19.py
+++ 25_test_cases/src/ex19.py
@@ -11,6 +11,18 @@ SPECIAL = '~!@#$%^&*()_+'                       # 13
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
 
-def gen_password(param):
-    # TODO : complete this
-    pass
+def gen_password(passlength):
+    # Fix : complete this
+    if passlength < 12:
+        passlength = 12
+    ranpass = []
+    ranpass.append(LOWER_LETTERS[random.randint(0, 25)])
+    ranpass.append(UPPER_LETTERS[random.randint(0, 25)])
+    ranpass.append(NUMBERS[random.randint(0, 9)])
+    ranpass.append(SPECIAL[random.randint(0, 12)])
+
+    while len(ranpass) < passlength:
+        ranpass.append(ALL_CHARS[random.randint(0, 74)])
+        random.shuffle(ranpass)
+
+    return ''.join(ranpass)
