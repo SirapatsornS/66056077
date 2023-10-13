44410a2bba67f5007ba742b0e88392aebb788f17
pink <66056077@kmitl.ac.th>
2023-10-13 22:01:20 +0700

    test ex21

--- 25_test_cases/src/ex21.py
+++ 25_test_cases/src/ex21.py
@@ -21,12 +21,12 @@ def is_valid_date(year, month, day):
     # Fix : complete this
     if not (1 <= month <= 12):
         return False
-    if is_leap_year(year) and month == 2 and day == 29:
+    elif is_leap_year(year) and month == 2 and day == 29:
         return True
-    if month in (1, 3, 5, 7, 8, 10, 12) and not (1 <= day <= 31):
+    elif month in (1, 3, 5, 7, 8, 10, 12) and not (1 <= day <= 31):
         return False
-    if month in (4, 6, 9, 11) and not (1 <= day <= 30):
+    elif month in (4, 6, 9, 11) and not (1 <= day <= 30):
         return False
-    if month == 2 and not (1 <= day <= 28):
+    elif month == 2 and not (1 <= day <= 28):
         return False
     return True
