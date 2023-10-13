588e2f00142c57d5d373278a36d887291f0662f5
pink <66056077@kmitl.ac.th>
2023-10-13 20:07:16 +0700

    test ex16

--- 25_test_cases/src/ex16.py
+++ 25_test_cases/src/ex16.py
@@ -3,7 +3,7 @@ Exercise 16
 """
 
 
-def mode(params):
+def mode(num_list):
     """
     Calculate the mode of a list of numbers.
 
@@ -13,4 +13,17 @@ def mode(params):
     Returns:
     - int or None: The mode of the list, or None if the list is empty.
     """
-    pass
+    len_list = num_list.__len__()
+    uniq_num = []
+    mode_num = []
+
+    if len_list > 0 :
+        for i in num_list:
+            if i not in uniq_num:
+                uniq_num.append(i)
+            else:
+                mode_num.append(i)
+        for n in mode_num :
+            return n
+    else :
+        return None
