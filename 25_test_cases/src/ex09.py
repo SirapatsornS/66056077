3b932b9b6bcc0e98c83516a88716aa949e4e3d28
pink <66056077@kmitl.ac.th>
2023-10-13 18:46:56 +0700

    test ex9

--- 25_test_cases/src/ex09.py
+++ 25_test_cases/src/ex09.py
@@ -19,7 +19,7 @@ def get_chess_square_color(col, row):
         str: The color of the chess square, either 'white' or 'black'.
     """
     # FIX : complete this
-    if col == row :
+    if (col + row)%2 == 0 :
         return 'White'
     else :
         return 'Black'
